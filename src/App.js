8089efd80bd1482825a53aee07e2c8d6133a40bd
prospersibanda <prosper.thecoder@gmail.com>
2024-09-02 15:20:39 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {RouterBrowser, Router, Routes, Route} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import ProductPage from './pages/ProductPage';
+import Payment from './pages/Payment';
+import Checkout from './pages/Checkout';
+import Bag from './pages/Bag';
+import Address from './pages/Address';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Dashboard/>} />
+            <Route path="/productpage" element={<ProductPage/>} />
+            <Route path="/bag" element={<Bag/>} />
+            <Route path="/checkout" element={<Checkout/>} />
+            <Route path="/address" element={<Address/>} />
+            <Route path="/payment" element={<Payment/>} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
