508da36f3a04c0b5acb33eb469ea8848f7b3e0b8
Zinhle <zinhle.nhlapo31@gmail.com>
2024-09-07 19:20:15 +0200

    Worked on the adress page.

--- src/pages/Address.jsx
+++ src/pages/Address.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react"; 
+import '../Styles/address.css';
 
 const Address = () => {
   return (
-    <div>Address</div>
-  )
+    <div className="address-container">
+      <form className="address-form">
+        <label htmlFor="name">Shipping Name</label> <br />
+        <input type="text" placeholder="John Maker" value="" /> <br /> <br />
+        <label htmlFor="street-name">Street Name</label> <br />
+        <input
+          type="text"
+          placeholder="123 Plae Grond Stret"
+          value=""
+        /> <br /> <br />
+        <label htmlFor="city">City</label> <br />
+        <input type="text" placeholder="Vermont" value="" /> <br /> <br />
+        <label htmlFor="state">State</label> <br />
+        <input type="text" placeholder="California" value="" /> <br /> <br />
+        <label htmlFor="country">Country</label> <br />
+        <input
+          type="text"
+          placeholder="United States of America"
+          value=""
+        />{" "}
+        <br />
+        <span>
+          <input type="checkbox" name="checkbox" value="" />{" "}
+          <p>Save this as your default address</p>
+        </span>{" "}
+        <br />
+        <input type="button" value="Add Address" /> <br/> <br/> <br/>
+        <span class="last-details">
+        <a href="/">Back</a>
+        <p class="connection">Secure Connection</p>
+        </span>
+      </form>
+    </div>
+  );
 }
 
-export default Address
+export default Address;
